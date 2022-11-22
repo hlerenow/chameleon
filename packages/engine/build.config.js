b1ea99166e78da8b63df9d5e2ec99c8cb09a584a
何雷 <595806119@qq.com>
2022-11-22 21:34:41 +0800

    fix(@chameleon/engine): fix app build script

--- packages/engine/build.config.js
+++ packages/engine/build.config.js
@@ -11,6 +11,7 @@ module.exports = {
     'react-dom': 'ReactDOM',
   },
   vite: {
+    base: process.env.BUILD_TYPE === 'APP' ? '/chameleon/' : '',
     build: {
       outDir: process.env.BUILD_TYPE === 'APP' ? './example' : './dist',
     },
