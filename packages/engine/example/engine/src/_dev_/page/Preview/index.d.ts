9252f4b0fdee443e63c8d2fde864758a1b9de2f2
何雷 <595806119@qq.com>
2023-01-25 13:10:31 +0800

    chore(@chameleon/engine): build example app

---
+++ packages/engine/example/engine/src/_dev_/page/Preview/index.d.ts
@@ -0,0 +1,2 @@
+/// <reference types="react" />
+export declare const Preview: () => JSX.Element;
---
+++ packages/engine/example/engine/src/plugins/DisplaySourceSchema/index.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+import { CPage } from '@chameleon/model';
+export declare type DisplaySourceSchemaProps = {
+    pageModel: CPage;
+    children: React.ReactNode;
+};
+export declare const DisplaySourceSchema: (props: DisplaySourceSchemaProps) => JSX.Element;
