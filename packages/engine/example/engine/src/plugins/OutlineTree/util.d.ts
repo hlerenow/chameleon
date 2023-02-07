250aae39cba5f38684f5eac960680cc581af4913
何雷 <595806119@qq.com>
2023-02-07 23:58:08 +0800

    chore(@chameleon/engine): build example app

--- packages/engine/example/engine/src/plugins/OutlineTree/util.d.ts
+++ packages/engine/example/engine/src/plugins/OutlineTree/util.d.ts
@@ -1,8 +1,9 @@
 import { DropPosType } from '@chameleon/layout/dist/components/DropAnchor/util';
-import { CPageDataType } from '@chameleon/model';
+import { CNodeDataType, CPage, CPageDataType } from '@chameleon/model';
 import { TreeNodeData } from './components/TreeView/dataStruct';
 export declare const getTargetMNodeKeyVal: (dom: HTMLElement | null, key: string) => null | string;
-export declare const transformPageSchemaToTreeData: (pageSchema: CPageDataType) => TreeNodeData[];
+export declare const transformNodeSchemaToTreeData: (nodeSchema: CNodeDataType | CNodeDataType[], parent: TreeNodeData, pageModel: CPage) => TreeNodeData | TreeNodeData[];
+export declare const transformPageSchemaToTreeData: (pageSchema: CPageDataType, pageModel: CPage) => TreeNodeData[];
 export declare const traverseTree: (tree: TreeNodeData | TreeNodeData[], handler: (node: TreeNodeData) => boolean) => void;
 export declare function calculateDropPosInfo(params: {
     point: {
