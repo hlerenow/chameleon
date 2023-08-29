b8e8c2c703f20c2bc2836dc2e1f91b02b994e5ca
何雷 <hlere@foxmail.com>
2023-08-29 22:26:45 +0800

    fix(engine): 🐛 fixed click can't add component into canvas
    affects: engine

--- packages/engine/src/plugins/ComponentLibrary/index.tsx
+++ packages/engine/src/plugins/ComponentLibrary/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { DOMAttributes } from 'react';
 import { AppstoreAddOutlined } from '@ant-design/icons';
 import { LayoutDragAndDropExtraDataType, Sensor, SensorEventObjType } from '@chamn/layout';
 import { Tabs } from 'antd';
@@ -9,7 +9,15 @@ import { withTranslation, WithTranslation } from 'react-i18next';
 import { ListView } from './components/ListView';
 import { getTargetMNodeKeyVal } from './util';
 import { DRAG_ITEM_KEY } from './components/DragItem';
-import { CNode, CRootNode, CSlot, findContainerNode, isPageModel, SnippetsCollection } from '@chamn/model';
+import {
+  CNode,
+  CRootNode,
+  CSlot,
+  DragAndDropEventExtraData,
+  findContainerNode,
+  isPageModel,
+  SnippetsCollection,
+} from '@chamn/model';
 import { capitalize } from 'lodash-es';
 import { InsertNodePosType } from '@chamn/model/src';
 import { DesignerPluginInstance } from '../Designer/type';
@@ -32,6 +40,7 @@ type ComponentLibViewState = {
 class ComponentLibView extends React.Component<ComponentLibViewProps, ComponentLibViewState> {
   containerRef: React.RefObject<HTMLDivElement>;
   disposeList: (() => void)[] = [];
+  boxSensor!: Sensor<DragAndDropEventExtraData>;
 
   constructor(props: ComponentLibViewProps) {
     super(props);
@@ -78,6 +87,26 @@ class ComponentLibView extends React.Component<ComponentLibViewProps, ComponentL
     this.disposeList.map((el) => el());
   }
 
+  getNewNode = (targetDom: HTMLElement | null) => {
+    const { pluginCtx } = this.props;
+    const pageModel = pluginCtx.pageModel;
+
+    if (!targetDom) {
+      return;
+    }
+    const targetNodeId = getTargetMNodeKeyVal(targetDom as HTMLElement, DRAG_ITEM_KEY);
+
+    if (!targetNodeId) {
+      return;
+    }
+
+    const meta = pageModel.materialsModel.findSnippetById(targetNodeId);
+    if (!meta) {
+      return;
+    }
+    return pageModel?.createNode(meta.schema);
+  };
+
   registerDragEvent = async () => {
     const { containerRef } = this;
     const { pluginCtx } = this.props;
@@ -89,7 +118,6 @@ class ComponentLibView extends React.Component<ComponentLibViewProps, ComponentL
     if (!containerRef.current) {
       return;
     }
-    const pageModel = pluginCtx.pageModel;
     const designerExport = designerHandle.export;
     const dnd = designerExport.getDnd()!;
     const boxSensor = new Sensor<LayoutDragAndDropExtraDataType>({
@@ -97,26 +125,10 @@ class ComponentLibView extends React.Component<ComponentLibViewProps, ComponentL
       container: containerRef.current,
     });
 
-    const getNewNode = (eventObj: SensorEventObjType | Omit<SensorEventObjType, 'pointer'>) => {
-      const targetDom = eventObj.event.target;
-      if (!targetDom) {
-        return;
-      }
-      const targetNodeId = getTargetMNodeKeyVal(targetDom as HTMLElement, DRAG_ITEM_KEY);
-
-      if (!targetNodeId) {
-        return;
-      }
-
-      const meta = pageModel.materialsModel.findSnippetById(targetNodeId);
-      if (!meta) {
-        return;
-      }
-      return pageModel?.createNode(meta.schema);
-    };
+    this.boxSensor = boxSensor;
 
     boxSensor.setCanDrag(async (eventObj) => {
-      const newNode = getNewNode(eventObj);
+      const newNode = this.getNewNode(eventObj.event.target as any);
 
       this.props.pluginCtx.pluginManager.get<DesignerPluginInstance>('Designer').then((designerHandle) => {
         const designerExport = designerHandle?.export;
@@ -145,92 +157,85 @@ class ComponentLibView extends React.Component<ComponentLibViewProps, ComponentL
       dnd.emitter.off('dragStart', dragStart);
     });
     dnd.emitter.on('dragStart', dragStart);
+  };
 
-    const toAddNewNode = async (eventObj: Omit<SensorEventObjType, 'pointer'>) => {
-      const newNode = getNewNode(eventObj);
-      if (!newNode) {
+  toAddNewNode: DOMAttributes<HTMLDivElement>['onClick'] = async (event) => {
+    const newNode = this.getNewNode(event.nativeEvent.target as HTMLElement);
+    if (!newNode) {
+      return;
+    }
+
+    const { pageModel } = this.props.pluginCtx;
+
+    return this.props.pluginCtx.pluginManager.get<DesignerPluginInstance>('Designer').then(async (designerHandle) => {
+      const designerExport = designerHandle?.export;
+      // 获取当前选中，如果存在，就插入到当前选中的下面，否则就插入到根节点下面
+      const selectedNodeId = designerExport?.getSelectedNodeId();
+      const selectedNode = pageModel.getNode(selectedNodeId);
+      const containerNode = findContainerNode(selectedNode);
+      let pos: InsertNodePosType = 'CHILD_END';
+      let dropNode: CNode | CRootNode | CSlot | null = selectedNode ?? null;
+      if (selectedNode) {
+        // 当前节点的父级节点是容器时
+        if (containerNode === selectedNode.parent) {
+          if (selectedNode.isContainer()) {
+            pos = 'CHILD_END';
+            dropNode = selectedNode;
+            pageModel.addNode(newNode, selectedNode as never, pos);
+          } else {
+            pos = 'AFTER';
+            dropNode = selectedNode;
+          }
+        } else if (containerNode && !isPageModel(containerNode)) {
+          pos = 'CHILD_END';
+          dropNode = containerNode;
+        }
+      } else {
+        const rootNode = pageModel.getRootNode();
+        if (rootNode) {
+          pos = 'CHILD_END';
+          dropNode = rootNode;
+        }
+      }
+      const designerInstance = designerExport?.getInstance();
+
+      const addFlag = await designerInstance?.customAdvanceHook.onNewAdd({
+        dragNode: newNode,
+        dropNode: dropNode as any,
+        eventObj: {
+          from: event.nativeEvent,
+          fromSensor: this.boxSensor,
+          pointer: null as any,
+          fromPointer: null as any,
+          extraData: {},
+        },
+      });
+      if (addFlag === false) {
         return;
       }
-
-      const { pageModel } = this.props.pluginCtx;
-
-      return this.props.pluginCtx.pluginManager.get<DesignerPluginInstance>('Designer').then(async (designerHandle) => {
-        const designerExport = designerHandle?.export;
-
-        // 获取当前选中，如果存在，就插入到当前选中的下面，否则就插入到根节点下面
-        const selectedNodeId = designerExport?.getSelectedNodeId();
-        const selectedNode = pageModel.getNode(selectedNodeId);
-        const containerNode = findContainerNode(selectedNode);
-        let pos: InsertNodePosType = 'CHILD_END';
-        let dropNode: CNode | CRootNode | CSlot | null = selectedNode ?? null;
-        if (selectedNode) {
-          // 当前节点的父级节点是容器时
-          if (containerNode === selectedNode.parent) {
-            if (selectedNode.isContainer()) {
-              pos = 'CHILD_END';
-              dropNode = selectedNode;
-              pageModel.addNode(newNode, selectedNode as never, pos);
-            } else {
-              pos = 'AFTER';
-              dropNode = selectedNode;
-            }
-          } else if (containerNode && !isPageModel(containerNode)) {
-            pos = 'CHILD_END';
-            dropNode = containerNode;
-          }
-        } else {
-          const rootNode = pageModel.getRootNode();
-          if (rootNode) {
-            pos = 'CHILD_END';
-            dropNode = rootNode;
-          }
-        }
-        const designerInstance = designerExport?.getInstance();
-
-        const addFlag = await designerInstance?.customAdvanceHook.onNewAdd({
-          dragNode: newNode,
-          dropNode: dropNode as any,
-          eventObj: {
-            ...eventObj,
-            from: eventObj.event,
-            fromSensor: boxSensor,
-            pointer: null as any,
-            fromPointer: null as any,
-            extraData: {},
-          },
-        });
-        if (addFlag === false) {
-          return;
-        }
-        let addNodeInfo = {
-          dragNode: newNode,
-          dropNode: dropNode,
-          pos: pos,
+      let addNodeInfo = {
+        dragNode: newNode,
+        dropNode: dropNode,
+        pos: pos,
+      };
+      if (typeof addFlag === 'object') {
+        addNodeInfo = {
+          ...addNodeInfo,
+          dragNode: (addFlag.addNode as any) ?? addNodeInfo.dragNode,
+          dropNode: (addFlag.dropNode as any) ?? addNodeInfo.dropNode,
+          pos: (addFlag.dropPosInfo?.pos as any) ?? addNodeInfo.pos,
         };
-        if (typeof addFlag === 'object') {
-          addNodeInfo = {
-            ...addNodeInfo,
-            dragNode: (addFlag.addNode as any) ?? addNodeInfo.dragNode,
-            dropNode: (addFlag.dropNode as any) ?? addNodeInfo.dropNode,
-            pos: (addFlag.dropPosInfo?.pos as any) ?? addNodeInfo.pos,
-          };
-        }
-        pageModel.addNode(addNodeInfo.dragNode, addNodeInfo.dropNode as CNode, addNodeInfo.pos);
-        setTimeout(() => {
-          designerExport?.selectNode(addNodeInfo.dragNode.id);
-        }, 200);
-      });
-    };
-
-    this.disposeList.push(() => {
-      boxSensor.emitter.off('onClick', toAddNewNode);
+      }
+      pageModel.addNode(addNodeInfo.dragNode, addNodeInfo.dropNode as CNode, addNodeInfo.pos);
+      setTimeout(() => {
+        designerExport?.selectNode(addNodeInfo.dragNode.id);
+      }, 200);
     });
-
-    boxSensor.emitter.on('onClick', toAddNewNode);
   };
 
   render(): React.ReactNode {
     const { componentsList } = this.state;
+    const { toAddNewNode } = this;
     const items = componentsList.map((el) => {
       return {
         label: <TabTitle>{capitalize(el.name)}</TabTitle>,
@@ -239,7 +244,7 @@ class ComponentLibView extends React.Component<ComponentLibViewProps, ComponentL
       };
     });
     return (
-      <div ref={this.containerRef} className={styles.container}>
+      <div ref={this.containerRef} className={styles.container} onClick={toAddNewNode}>
         <Tabs
           defaultActiveKey="BaseComponent"
           items={items}
