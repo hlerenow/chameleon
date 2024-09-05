c82bd21243aed9371a8576f572f600f1894f60bf
何雷 <hlere@foxmail.com>
2024-09-05 21:50:50 +0800

    feat(engine): 🎸 add hotAction
    affects: engine

--- packages/engine/src/plugins/Hotkeys/index.ts
+++ packages/engine/src/plugins/Hotkeys/index.ts
@@ -1,5 +1,7 @@
+import { CNode } from '@chamn/model';
 import { CPlugin } from '../../core/pluginManager';
 import { DesignerPluginInstance } from '../Designer/type';
+import { HistoryPluginInstance } from '../History/type';
 import { HotKeysManager } from './hotKeyManager';
 import localize from './localize';
 
@@ -14,6 +16,7 @@ export const HotkeysPlugin: CPlugin = {
     Object.keys(localize).forEach((lng) => {
       i18n.addResourceBundle(lng, i18nNamespace, localize[lng], true, true);
     });
+    const historyStack = await ctx.pluginManager.get<HistoryPluginInstance>('History');
 
     const designer = await ctx.pluginManager.get<DesignerPluginInstance>('Designer');
 
@@ -31,7 +34,10 @@ export const HotkeysPlugin: CPlugin = {
     });
 
     hotkeyManager.addHotAction(['ctrl', 'c'], () => {
-      console.log('复制');
+      const activeNode = ctx.engine.getActiveNode();
+      if (activeNode) {
+        ctx.pageModel.copyNodeById(activeNode.id);
+      }
     });
 
     hotkeyManager.addHotAction([17, 16, 90], () => {
@@ -39,39 +45,88 @@ export const HotkeysPlugin: CPlugin = {
     });
 
     hotkeyManager.addHotAction([17, 90], () => {
-      console.log('undo');
+      if (historyStack?.export.canGoPreStep()) {
+        historyStack?.export.preStep();
+      }
     });
 
-    hotkeyManager.addHotAction([38], () => {
-      console.log('up');
+    hotkeyManager.addHotAction(['up'], () => {
+      const activeNode = ctx.engine.getActiveNode();
+      const parentNode = activeNode?.parent;
+      if (parentNode && parentNode.nodeType !== 'PAGE') {
+        designer?.export?.selectNode(parentNode.id);
+      }
     });
 
-    hotkeyManager.addHotAction([87], () => {
-      console.log('up');
+    hotkeyManager.addHotAction(['w'], () => {
+      const activeNode = ctx.engine.getActiveNode();
+      const parentNode = activeNode?.parent;
+      if (parentNode && parentNode.nodeType !== 'PAGE') {
+        designer?.export?.selectNode(parentNode.id);
+      }
     });
 
-    hotkeyManager.addHotAction([16, 87], () => {
-      console.log('sibling up');
+    hotkeyManager.addHotAction(['shift', 'w'], () => {
+      const activeNode = ctx.engine.getActiveNode();
+      const parentNode = activeNode?.parent;
+      if (parentNode?.nodeType === 'PAGE' || !activeNode) {
+        return;
+      }
+      const parentNodeValue = parentNode?.value as any;
+      const children: CNode[] = parentNodeValue.children || parentNodeValue.value;
+      const targetIndex = children.findIndex((el) => el.id === activeNode.id);
+      const nextIndex = targetIndex - 1;
+      if (nextIndex < 0) {
+        return;
+      }
+      const child = children?.[nextIndex] as CNode;
+      if (child && (child as any).id) {
+        designer?.export?.selectNode(child.id);
+      }
     });
 
-    hotkeyManager.addHotAction([40], () => {
-      console.log('down');
+    hotkeyManager.addHotAction(['down'], () => {
+      const activeNode = ctx.engine.getActiveNode();
+      const children = activeNode?.value.children;
+      const child = children?.[0] as CNode;
+      if (child && (child as any).id) {
+        designer?.export?.selectNode(child.id);
+      }
     });
 
-    hotkeyManager.addHotAction([83], () => {
-      console.log('down');
+    hotkeyManager.addHotAction(['s'], () => {
+      const activeNode = ctx.engine.getActiveNode();
+      const children = activeNode?.value.children;
+      const child = children?.[0] as CNode;
+      if (child && (child as any).id) {
+        designer?.export?.selectNode(child.id);
+      }
     });
 
-    hotkeyManager.addHotAction([16, 83], () => {
-      console.log('sibling down');
-    });
-
-    hotkeyManager.addHotAction([83], () => {
-      console.log('down');
+    hotkeyManager.addHotAction(['shift', 's'], () => {
+      const activeNode = ctx.engine.getActiveNode();
+      const parentNode = activeNode?.parent;
+      if (parentNode?.nodeType === 'PAGE' || !activeNode) {
+        return;
+      }
+      const parentNodeValue = parentNode?.value as any;
+      const children: CNode[] = parentNodeValue.children || parentNodeValue.value;
+      const targetIndex = children.findIndex((el) => el.id === activeNode.id);
+      const nextIndex = targetIndex + 1;
+      if (nextIndex >= children.length) {
+        return;
+      }
+      const child = children?.[nextIndex] as CNode;
+      if (child && (child as any).id) {
+        designer?.export?.selectNode(child.id);
+      }
     });
 
     hotkeyManager.addHotAction(['backspace'], () => {
-      console.log('delete');
+      const activeNode = ctx.engine.getActiveNode();
+      if (activeNode) {
+        ctx.pageModel.deleteNode(activeNode);
+      }
     });
 
     (ctx as any).hotkeyManager = hotkeyManager;
