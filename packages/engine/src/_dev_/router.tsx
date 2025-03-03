0fcab5b5ad715762327c26d3f7eae4680604644b
何雷 <hlere@foxmail.com>
2025-03-03 22:25:34 +0800

    feat(engine): 🎸 add workbench widget control config
    affects: engine

--- packages/engine/src/_dev_/router.tsx
+++ packages/engine/src/_dev_/router.tsx
@@ -1,8 +1,13 @@
 import { createHashRouter } from 'react-router-dom';
 import { App } from './page/Editor';
 import { Preview } from './page/Preview';
+import { ComponentEditor } from './page/componentEditor';
 
 export const router: any = createHashRouter([
+  {
+    path: '/previewComp',
+    element: <ComponentEditor />,
+  },
   {
     path: '/',
     element: <App />,
